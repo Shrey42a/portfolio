d0dd9349928bb7a04800179db025109bb18e49f0
Shrey Kumar <akashjaat55242@gmail.com>
2023-03-24 13:57:07 +0530

    2nd

--- src/More.jsx
+++ src/More.jsx
@@ -8,7 +8,7 @@ function More() {
             <div className="h-max bg20 w-full">
                 <Navbar />
                 <div className="lg:pt-14 h-full w-full lg:px-8 lg:py-4 p-2 flex flex-col justify-center items-center">
-                    <div className="flex h-full flex-wrap justify-evenly gap-2 p-2 bg-white/10 rounded-lg">
+                    <div className="flex h-full flex-wrap justify-evenly gap-2 p-3 bg-white/10 rounded-lg">
                         <div class="h-[24rem] w-[24rem] rounded-lg bg-white/10 p-2 shadow-lg shadow-zinc-800"></div>
                         <div class="h-[24rem] w-[24rem] rounded-lg bg-white/10 p-2 shadow-lg shadow-zinc-800"></div>
                         <div class="h-[24rem] w-[24rem] rounded-lg bg-white/10 p-2 shadow-lg shadow-zinc-800"></div>
