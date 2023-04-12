325aab8265048c2015c864ba74325bf1ac6a004e
Shrey Kumar <akashjaat55242@gmail.com>
2023-04-12 13:09:08 +0530

    10th

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,26 +2,10 @@ import React from "react";
 import { Link } from "react-scroll";
 
 function Header() {
-  (function () {
-    document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector("#parallax");
-    function parallax(e) {
-      let _w = window.innerWidth / 2;
-      let _h = window.innerHeight / 2;
-      let _mouseX = e.clientX;
-      let _mouseY = e.clientY;
-      let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-      let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-      elem = x;
-    }
-
-  })();
   return (
     <>
       <div className="flex bg1 h-screen w-full justify-center lg:items-center">
-        <div id="parallax" className="min-w-fit justify-center items-center flex flex-col">
+        <div className="min-w-fit justify-center items-center flex flex-col">
           <div className="flex flex-col items-center justify-center space-y-2">
             <h1 className="font-mono text-white text-2xl lg:relative lg:right-96">Hello there, this is</h1>
             <div data-splitting="lines" className=" reflected">
